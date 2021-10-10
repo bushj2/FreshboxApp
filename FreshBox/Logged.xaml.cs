7b3cd515350d0690d6d7242a00c5324d33270104
Jack Bush <bush.j.2018@gmail.com>
2021-10-10 11:42:37 -0400

    changed to popup pages instead of content

---
+++ FreshBox/Logged.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.CommunityToolkit.Extensions;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+
+namespace FreshBox
+{
+    public partial class Logged : Popup
+    {
+        public Logged()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FreshBox/SignUp.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms;
+
+namespace FreshBox
+{
+    public partial class SignUp : Popup
+    {
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+    }
+}
